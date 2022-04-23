3ad2ab9fcf955737a0724e9b882a2098adcde4d0
sugasugary <95432496+sugasugary@users.noreply.github.com>
2022-04-23 14:26:16 +0700

    Add files via upload

---
+++ eunoia47youtubeamnubis.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.popup-youtube').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
